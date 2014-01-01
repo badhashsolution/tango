b9695fb62887990a7fde03f986bc803fe937b13b
badhashsolution <badhashsolution@gmail.com>
2014-01-01 13:11:09 -0800

    Added index.html

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,7 +1,22 @@
 from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render_to_response
 
 def index(request):
-    return HttpResponse("Rango Says: Hello World!! <a href='/rango/about'>About</a>")
+    # Request the context of the request.
+    # The context contains information such as the client's
+    # machine details, for example.
+    context = RequestContext(request)
+
+    # Construct a dictionary to pass to the template engine
+    # as its context. Note the key boldmessage is the same
+    # as {{ boldmessage }} in the template!
+    context_dict = {'boldmessage': "I am bold font from the context"}
+
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+    return render_to_response('rango/index.html', context_dict, context)
 
 def about(request):
     return HttpResponse("Rango Says: Here is the about page. <a href='/rango/'>Index</a>")
