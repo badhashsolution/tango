d192a102b00ebcca9d5433d6d55c3733b1e0f80e
badhashsolution <badhashsolution@gmail.com>
2014-01-01 18:43:25 -0800

    finishing section 4

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -19,4 +19,6 @@ def index(request):
     return render_to_response('rango/index.html', context_dict, context)
 
 def about(request):
-    return HttpResponse("Rango Says: Here is the about page. <a href='/rango/'>Index</a>")
+    context = RequestContext(request)
+    context_dict = {'boldmessage': "This is the about page"}
+    return render_to_response('rango/about.html', context_dict, context)
