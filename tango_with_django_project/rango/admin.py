680882635ed9718f6dee434472c5d958f0f28a0c
badhashsolution <badhashsolution@gmail.com>
2014-01-01 20:53:20 -0800

    finished lesson 5

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
 # WGG Register models with the admin interface
 admin.site.register(Category)
-admin.site.register(Page)
+admin.site.register(Page, PageAdmin)
+
+
 
--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
 
     def __unicode__(self):
         return self.name
